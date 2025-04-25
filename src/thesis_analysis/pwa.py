hopefully fix some scaling issues

src/thesis_analysis/pwa.py
@@ -455,8 +455,9 @@ def fit_binned(
         best_nll = np.inf
         best_status = None
         rng = np.random.default_rng(0)
+        init_mag = 1000.0
         for iiter in range(iters):
-            p_init = rng.uniform(-1000.0, 1000.0, len(nll.parameters))
+            p_init = rng.uniform(-init_mag, init_mag, len(nll.parameters))
             status = nll.minimize(
                 p_init,
                 observers=[LoggingObserver()],
@@ -622,7 +623,7 @@ def fit_unbinned(
     best_nll = np.inf
     best_status = None
     rng = np.random.default_rng(0)
-    init_mag = 0.0001 if phase_factor else 1000.0
+    init_mag = 1000.0
     for iiter in range(iters):
         p_init = (
             p0
@@ -1189,11 +1190,7 @@ def fit_guided(
     best_nll = np.inf
     best_status = None
     rng = np.random.default_rng(0)
-    init_mag = (
-        0.0001
-        if binned_fit_result_uncertainty.fit_result.phase_factor
-        else 1000.0
-    )
+    init_mag = 1000.0
     for _ in range(iters):
         p_init = (
             p0
@@ -1306,8 +1303,9 @@ def fit_binned_regularized(
         best_nll = np.inf
         best_status = None
         rng = np.random.default_rng(0)
+        init_mag = 1000.0
         for iiter in range(iters):
-            p_init = rng.uniform(-1000.0, 1000.0, len(nll.parameters))
+            p_init = rng.uniform(-init_mag, init_mag, len(nll.parameters))
             status = nll.minimize(
                 p_init,
                 observers=[LoggingObserver()],