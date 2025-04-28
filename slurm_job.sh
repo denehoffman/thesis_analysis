8963c46444b89640f76966505620b597eff49a23
denehoffman <denehoffman@me.com>
2025-04-28 12:24:46 -0400

    update init and slurm job settings

--- slurm_job.sh
+++ slurm_job.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=dene-thesis
 #SBATCH --output=slurm.log
 #SBATCH --error=slurm.log
-#SBATCH --ntasks=1
+#SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=120G
+#SBATCH --mem=120G
 #SBATCH --partition=blue
 
 source /home/nhoffman/.venv313/bin/activate
@@ -12,5 +12,6 @@ source /raid3/nhoffman/root/root_install_313/bin/thisroot.sh
 
 cd "$PWD"
 uv pip install --reinstall .
+python -c "import laddu; print(f'Cores available: {laddu.available_parallelism()}')"
 luigid --background --pidfile ./luigi.pid --logdir ./luigi.log &
-GLUEX_USERNAME=nhoffman GLUEX_HOSTNAME=ernest.phys.cmu.edu NUM_THREADS=64 luigi --module thesis_analysis RunAll --workers=32
+GLUEX_USERNAME=nhoffman GLUEX_HOSTNAME=ernest.phys.cmu.edu NUM_THREADS=32 luigi --module thesis_analysis RunAll --workers=32
--- src/thesis_analysis/__init__.py
+++ src/thesis_analysis/__init__.py
@@ -20,112 +20,79 @@ from thesis_analysis.wave import Wave
 
 def run_all(chisqdof: float) -> list[luigi.Task]:
     return [
-        # RFLPlot('data', chisqdof=chisqdof),
-        # RFLPlot('accmc', chisqdof=chisqdof),
-        # RFLPlot('bkgmc', chisqdof=chisqdof),
-        # ChiSqDOFPlot('data', bins=50),
-        # MassPlot('data', bins=50),
-        # ChiSqDOFPlot('accmc', bins=50),
-        # ChiSqDOFPlot('data', bins=50, chisqdof=chisqdof),
-        # MassPlot('data', bins=50, chisqdof=chisqdof),
-        # ChiSqDOFPlot('accmc', bins=50, chisqdof=chisqdof),
-        # ChiSqDOFPlot(
-        #     'data',
-        #     bins=50,
-        #     chisqdof=chisqdof,
-        #     splot_method='D',
-        #     nsig=1,
-        #     nbkg=2,
-        # ),
-        # MassPlot(
-        #     'data',
-        #     bins=50,
-        #     chisqdof=chisqdof,
-        #     splot_method='D',
-        #     nsig=1,
-        #     nbkg=2,
-        # ),
-        # CosThetaPlot(
-        #     'data',
-        #     bins=50,
-        #     chisqdof=chisqdof,
-        #     splot_method='D',
-        #     nsig=1,
-        #     nbkg=2,
-        # ),
-        # Regularized Fits
+        RFLPlot('data', chisqdof=chisqdof),
+        RFLPlot('accmc', chisqdof=chisqdof),
+        RFLPlot('bkgmc', chisqdof=chisqdof),
+        ChiSqDOFPlot('data', bins=50),
+        MassPlot('data', bins=50),
+        ChiSqDOFPlot('accmc', bins=50),
+        ChiSqDOFPlot('data', bins=50, chisqdof=chisqdof),
+        MassPlot('data', bins=50, chisqdof=chisqdof),
+        ChiSqDOFPlot('accmc', bins=50, chisqdof=chisqdof),
+        ChiSqDOFPlot(
+            'data',
+            bins=50,
+            chisqdof=chisqdof,
+            splot_method='D',
+            nsig=1,
+            nbkg=2,
+        ),
+        MassPlot(
+            'data',
+            bins=50,
+            chisqdof=chisqdof,
+            splot_method='D',
+            nsig=1,
+            nbkg=2,
+        ),
+        CosThetaPlot(
+            'data',
+            bins=50,
+            chisqdof=chisqdof,
+            splot_method='D',
+            nsig=1,
+            nbkg=2,
+        ),
+        # Fits
         *[
-            BinnedRegularizedPlot(
-                waves=Wave.encode_waves(waves),
+            BinnedAndUnbinnedPlot(
+                waves=waves,
+                chisqdof=chisqdof,
+                splot_method='D',
+                nsig=1,
+                nbkg=2,
+                niters=5,
+                guided=guided,
+                phase_factor=True,
+                uncertainty='bootstrap',
+                bootstrap_mode='SE',
+            )
+            for waves in [
+                Wave.encode_waves([Wave(0, 0, '+'), Wave(2, 2, '+')]),
+                Wave.encode_waves(
+                    [Wave(0, 0, '+'), Wave(0, 0, '-'), Wave(2, 2, '+')]
+                ),
+            ]
+            for guided in [True, False]
+        ],
+        *[
+            BinnedPlot(
+                waves=waves,
                 chisqdof=chisqdof,
                 splot_method='D',
                 nsig=1,
                 nbkg=2,
                 niters=1,
                 phase_factor=True,
-                lda=lda,
-                gamma=gamma,
+                uncertainty='bootstrap',
+                bootstrap_mode='CI-BC',
             )
-            for lda in [1.0, 0.1, 0.01, 0.001, 0.0]
-            for gamma in [0.5, 1.0, 2.0]
             for waves in [
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 2, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 1, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 0, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 1, '+'),
-                    Wave(2, 2, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 0, '+'),
-                    Wave(2, 2, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 0, '+'),
-                    Wave(2, 1, '+'),
-                ],
-                [
-                    Wave(0, 0, '+'),
-                    Wave(2, 0, '+'),
-                    Wave(2, 1, '+'),
-                    Wave(2, 2, '+'),
-                ],
+                Wave.encode_waves([Wave(0, 0, '+'), Wave(2, 2, '+')]),
+                Wave.encode_waves([Wave(0, 0, '+'), Wave(2, 1, '+')]),
+                Wave.encode_waves([Wave(0, 0, '+'), Wave(2, 0, '+')]),
             ]
         ],
-        # Fits
-        # *[
-        #     BinnedAndUnbinnedPlot(
-        #         waves=waves,
-        #         chisqdof=chisqdof,
-        #         splot_method='D',
-        #         nsig=1,
-        #         nbkg=2,
-        #         niters=1,
-        #         guided=guided,
-        #         phase_factor=True,
-        #         uncertainty='bootstrap',
-        #         bootstrap_mode='SE',
-        #     )
-        #     for waves in [
-        #         Wave.encode_waves([Wave(0, 0, '+'), Wave(2, 2, '+')]),
-        #         Wave.encode_waves(
-        #             [Wave(0, 0, '+'), Wave(0, 0, '-'), Wave(2, 2, '+')]
-        #         ),
-        #     ]
-        #     for guided in [True, False]
-        # ],
     ]
 
 
@@ -133,24 +100,24 @@ class RunAll(luigi.WrapperTask):
     @override
     def requires(self):
         return [
-            # *run_all(3.0),
+            *run_all(1.4),
+            *run_all(2.4),
             *run_all(3.4),
-            # *run_all(4.0),
-            # *run_all(5.0),
-            # *run_all(6.0),
-            # MakeAuxiliaryPlots(),
-            # BGGENPlots(run_period='s18'),
-            # CutPlotsCombined(data_type='data_original'),
-            # CutPlotsCombined(data_type='data_original', chisqdof=3.4),
-            # CutPlotsCombined(data_type='data_original', protonz=True),
-            # CutPlotsCombined(
-            #     data_type='data_original', chisqdof=3.4, protonz=True
-            # ),
-            # RFPlot(data_type='data_original'),
-            # RFPlot(data_type='data_original', chisqdof=3.4),
-            # RFPlot(data_type='data_original', protonz=True),
-            # RFPlot(data_type='data_original', chisqdof=3.4, protonz=True),
-            # CutPlotsCombined(data_type='data'),
-            # FactorizationReport(3.4, max_quantiles=4),
-            # SPlotReport(3.4, nsig_max=1, nbkg_max=4),
+            *run_all(4.4),
+            *run_all(5.4),
+            MakeAuxiliaryPlots(),
+            BGGENPlots(run_period='s18'),
+            CutPlotsCombined(data_type='data_original'),
+            CutPlotsCombined(data_type='data_original', chisqdof=3.4),
+            CutPlotsCombined(data_type='data_original', protonz=True),
+            CutPlotsCombined(
+                data_type='data_original', chisqdof=3.4, protonz=True
+            ),
+            RFPlot(data_type='data_original'),
+            RFPlot(data_type='data_original', chisqdof=3.4),
+            RFPlot(data_type='data_original', protonz=True),
+            RFPlot(data_type='data_original', chisqdof=3.4, protonz=True),
+            CutPlotsCombined(data_type='data'),
+            FactorizationReport(3.4, max_quantiles=4),
+            SPlotReport(3.4, nsig_max=1, nbkg_max=4),
         ]
